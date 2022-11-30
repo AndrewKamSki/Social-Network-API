3f2e9fa85bf3d8453181294054e0c69e62384879
Andy Kaminski <andy.kaminski42@gmail.com>
2022-11-30 09:54:22 -0500

    Added user routes and controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,67 @@
 const { User, Thought } = require('../models');
 
 const userController = {
- 
+  // Get all users
+  getAllUsers(req, res) {
+    User.find()
+    .populate({path: 'thoughts', select: '-__v'})
+    .populate({path: 'friends', select: '-__v'})
+    .then(userData => res.json(userData))
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json(err);
+    });
+  },
+
+  // Get a single user by _id w/ populated thought and friend data
+  getUserById(req, res) {
+    User.findOne({ _id: req.params.userId })
+    .populate({path: 'thoughts', select: '-__v'})
+    .populate({path: 'friends', select: '-__v'})
+    .then(userData => res.json(userData))
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json(err);
+    });
+  },
+
+  // Post a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((userData) => res.json(userData))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // Update a user by its _id
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((userData) =>
+        !userData
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(userData)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Delete a user by its _id
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((userData) =>
+        !userData
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : Thought.deleteMany({ _id: { $in: userData.thoughts } })
+      )
+      .then(() => res.json({ message: 'User deleted!' }))
+      .catch((err) => res.status(500).json(err));
+  },
+
 };
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
 const {
   // Exported functions from user controller
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
 } = require('../../controllers/user-controller');
 
 // Use exported functions from line 3 to create routes
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
 
 module.exports = router;
