90fbcd51af1b9f752e10e39b8573355ba282305e
Andy Kaminski <andy.kaminski42@gmail.com>
2022-11-29 19:52:34 -0500

    Committing last unsaved changes to models

--- models/Reaction.js
+++ models/Reaction.js
@@ -2,7 +2,32 @@ const { Schema, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const reactionSchema = new Schema(
-  
+  {
+    // Set custom ID 
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: dateFormat
+    }
+  },
+  {
+    toJSON: {
+        getters: true
+    } 
+  }
 );
 
 // We only need to make the schema for this one
--- models/Thought.js
+++ models/Thought.js
@@ -3,11 +3,38 @@ const reactionSchema = require('./Reaction');
 const dateFormat = require('../utils/dateFormat');
 
 const thoughtSchema = new Schema(
- 
+  {
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: dateFormat
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+  }
 );
 
-// Add virtual
-
+// virtual
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
 // Make model of Thought
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
